5fe12acf0de785c41cd863b8e287c0e2d05e880d
fuu <fu@yihaofang.com>
2022-09-07 17:23:23 +0800

    Load cApp require lib function

---
+++ src/lib/cloader.js
@@ -0,0 +1,8 @@
+//autoload JS and CSS from anchor network
+//regroup the code and order
+
+const cloader = {
+
+}
+
+export default cloader;
--- src/preter/app.js
+++ src/preter/app.js
@@ -6,29 +6,42 @@ import RPC from '../lib/rpc.js';
 
 function AnchorApp(props) {
     const self={
-        getLibs:(list,ck,cache)=>{
-            console.log(`Start:${JSON.stringify(list)}`);
+        getLibs:(list,ck,cache,order)=>{
+            //console.log(`Start:${JSON.stringify(list)}`);
             if(!cache) cache={};
+            if(!order) order=[];
             const row=list.shift();
             const anchor=(Array.isArray(row)?row[0]:row).toLocaleLowerCase();
             const block=Array.isArray(row)?row[1]:0;
 
-            if(cache[anchor]) return self.getLibs(list,ck,cache);
-
+            //1.check lib loading status
+            if(cache[anchor]) return self.getLibs(list,ck,cache,order);
 
             //2.get target anchor
             self.getAnchor(anchor,block,(an,res)=>{
                 cache[an]=!res?{error:'no such anchor'}:res;
+                if(!res.protocol || (!res.protocol.ext && !res.protocol.lib)){
+                    order.push(an);
+                }else{
+                    //console.log('ready to solve complex order');
+                    const qu={
+                        entry:an,
+                        lib:res.protocol && res.protocol.lib?res.protocol.lib:[],
+                        ext:res.protocol && res.protocol.ext?res.protocol.ext:[],
+                    };
+                    order.push(qu);
+                }
+
                 if(res.protocol && res.protocol.ext){
                     for(let i=res.protocol.ext.length;i>0;i--) list.unshift(res.protocol.ext[i-1]);
                 }
-
                 if(res.protocol && res.protocol.lib){
-                    for(let i=0;i<res.protocol.lib.length;i++) list.unshift(res.protocol.lib[i]);
+                    for(let i=res.protocol.lib.length;i>0;i--) list.unshift(res.protocol.lib[i-1]);
+                    //for(let i=0;i<res.protocol.lib.length;i++) list.unshift(res.protocol.lib[i]);
                 }
 
-                if(list.length===0) return ck && ck(cache);
-                self.getLibs(list,ck,cache);
+                if(list.length===0) return ck && ck(cache,order);
+                self.getLibs(list,ck,cache,order);
             });
         },
         getAnchor:(anchor,block,ck)=>{
@@ -43,31 +56,236 @@ function AnchorApp(props) {
                 });
             });
         },
-        loadLib:(list,ck)=>{
-            const scp = document.createElement('script');
-            scp.type = 'text/javascript';
-            scp.crossOrigin = 'anonymous';
-            let txt='';
-            self.getLibs(list,(dt)=>{
-                console.log(dt);
-                const failed={};
-                const len=list.length;
-                for(let i=0;i<len;i++){
-                    const row=list[i];
-                    if(Array.isArray(row)){
-                        txt+=!dt[row[0]]?'':dt[row[0]];
-                    }else{
-                        txt+=!dt[row]?'':dt[row];
+        decodeLib:(dt)=>{
+            //console.log(dt);
+            const result={type:'error',data:''};
+            if(dt.error){
+                result.error=dt.error;
+                return result;
+            }
+
+            if(!dt.protocol){
+                result.error='Unexcept data format';
+                return result;
+            }
+
+            const proto=dt.protocol;
+            if(!proto.format){
+                result.error='Anchor format lost';
+                return result;
+            }
+            result.type=proto.format;
+            result.data=dt.raw;
+            return result;
+        },
+        
+        getComplexOrder:(name,map,queue,hold)=>{
+            //console.log(map);
+            //console.log(`Working on ${name},hold ${JSON.stringify(hold)}`);
+            if(!queue) queue=[];        //获取的
+            if(!hold) hold=[];          //1.用来表达处理状态
+
+            if(map[name]===true && hold.length===0) return queue;
+            const row=map[name];
+
+            const last=hold.length!==0?hold[hold.length-1]:null;
+            const recover=(last!==null&&last.name===name)?hold.pop():null;
+
+            //if(recover!==null){
+                //console.log(`Recover:${JSON.stringify(recover)},hold:${JSON.stringify(hold)}`);
+                //console.log(`Name:${name},queue:${JSON.stringify(queue)}`);
+                //return false;
+            //}
+
+            //1.check lib complex;
+            if(row.lib && row.lib.length>0){
+                if(recover===null){
+                    for(let i=0;i<row.lib.length;i++){
+                        const lib=row.lib[i];
+                        //console.log('ready to check lib:'+lib);
+                        if(map[lib]===true){
+                            queue.push(lib);
+                        }else{
+                            hold.push({lib:i,name:name});
+                            return self.getComplexOrder(lib,map,queue,hold);
+                        }
+                    }
+                }else{
+                    if(recover.lib!==undefined && recover.lib!==row.lib.length){
+                        for(let i=recover.lib+1;i<row.lib.length;i++){
+                            const lib=row.lib[i];
+                            //console.log('ready to check lib:'+lib);
+                            if(map[lib]===true){
+                                queue.push(lib);
+                            }else{
+                                hold.push({lib:i,name:name});
+                                return self.getComplexOrder(lib,map,queue,hold);
+                            }
+                        }
                     }
                 }
+            }
 
-                scp.text=txt;
-                if (document.head.append) {
-                    document.head.append(scp);
-                } else {
-                    document.getElementsByTagName('head')[0].appendChild(scp);
+            if(recover===null) queue.push(name);
+
+            //2.check extend complex;
+            if(row.ext && row.ext.length>0){
+                if(recover!==null){
+                    if(recover.ext!==undefined && recover.ext!==row.ext.length){
+                        for(let i=recover.ext+1;i<row.ext.length;i++){
+                            const ext=row.ext[i];
+                            //console.log('ready to check lib:'+ext);
+                            if(map[ext]===true){
+                                queue.push(ext);
+                            }else{
+                                hold.push({ext:i,name:name});
+                                return self.getComplexOrder(ext,map,queue,hold);
+                            }
+                        }
+                    }
+                }else{
+                    for(let i=0;i<row.ext.length;i++){
+                        const ext=row.ext[i];
+                        //console.log('ready to check lib:'+ext);
+                        if(map[ext]===true){
+                            queue.push(ext);
+                        }else{
+                            hold.push({ext:i,name:name});
+                            return self.getComplexOrder(ext,map,queue,hold);
+                        }
+                    }
                 }
-                ck && ck(failed);
+            }
+
+            if(hold.length!==0){
+                //console.log('ready to recover');
+                //recover from interrupt
+                const last=hold[hold.length-1];
+                return self.getComplexOrder(last.name,map,queue,hold);
+            }
+
+            return queue;
+        },
+        mergeOrder:(order)=>{
+            //console.log(order);
+            const complex={};
+            const map={};
+            const done={};
+            const queue=[];
+            for(let i=0;i<order.length;i++){
+                const row=order[i];
+                if (typeof row !== 'string' && row.entry!==undefined){
+                    complex[row.entry]=true;
+                    map[row.entry]=row;
+                }else{
+                    map[row]=true;
+                }
+            }
+
+            for(let i=0;i<order.length;i++){
+                const row=order[i];
+                if (typeof row === 'string' || row instanceof String){
+                    if(done[row]) continue;
+                    queue.push(row);
+                    done[row]=true;
+                }else{
+                    //2.complex lib
+                    //2.1.add required libs
+                    if(row.lib && row.lib.length>0){
+                        for(let i=0;i<row.lib.length;i++){
+                            const lib=row.lib[i];
+                            if(done[lib]) continue;
+                            if(complex[lib]){
+                                const cqueue=self.getComplexOrder(lib,map);
+                                console.log(`${lib}:${JSON.stringify(cqueue)}`)
+                                for(let j=0;j<cqueue.length;j++){
+                                    const clib=cqueue[j];
+                                    if(done[clib]) continue;
+                                    queue.push(clib);
+                                    done[clib]=true;
+                                }
+                            }else{
+                                queue.push(lib);
+                                done[lib]=true;
+                            }
+                        }
+                    }
+                    //2.2.add lib body
+                    if(!done[row.entry]){
+                        queue.push(row.entry);
+                        done[row.entry]=true;
+                    }
+
+                    //2.3.add required extend plugins
+                    if(row.ext && row.ext.length>0){
+                        for(let i=0;i<row.ext.length;i++){
+                            const ext=row.ext[i];
+                            if(done[ext]) continue;
+                            if(complex[ext]){
+                                const cqueue=self.getComplexOrder(ext,map);
+                                for(let j=0;j<cqueue.length;j++){
+                                    const cext=cqueue[j];
+                                    if(done[cext]) continue;
+                                    queue.push(cext);
+                                    done[cext]=true;
+                                }
+                            }else{
+                                queue.push(ext);
+                                done[ext]=true;
+                            }
+                        }
+                    }
+                }
+            }
+            //console.log(`Result:${JSON.stringify(queue)},complex lib:${JSON.stringify(complex)}`);
+            return queue;
+        },
+        regroupCode:(map,order)=>{
+            console.log(map);
+            
+            const decode=self.decodeLib;
+            let js='';
+            let css='';
+            let done={};
+            let failed={};
+
+            const ods=self.mergeOrder(order);
+            console.log(`Plain lib array: ${JSON.stringify(ods)}`);
+            for(let i=0;i<ods.length;i++){
+                const row=ods[i];
+                if(done[row]) continue;
+                const dt=map[row];
+                const res=decode(dt);
+                done[row]=true;
+                if(res.error){
+                    failed[row]=res.error;
+                    continue;
+                }
+                js+=res.type==="JS"?res.data:'';
+                css+=res.type==="CSS"?res.data:'';
+            }
+            return {js:js,css:css,failed:failed,order:ods};
+        },
+        loadLib:(list,ck)=>{
+            self.getLibs(list,(dt,order)=>{                
+                const code=self.regroupCode(dt,order);
+                console.log(code.js);
+                console.log(code.css);
+                console.log(JSON.stringify(code.failed));
+                console.log(JSON.stringify(code.order));
+                
+                const scp = document.createElement('script');
+                scp.type = 'text/javascript';
+                scp.crossOrigin = 'anonymous';
+
+
+                // scp.text=txt;
+                // if (document.head.append) {
+                //     document.head.append(scp);
+                // } else {
+                //     document.getElementsByTagName('head')[0].appendChild(scp);
+                // }
+                ck && ck(code.failed);
             });
         },
     }
